f42353ef69f8d8d1afaa96208582c89a499c3c49
bennyocean <benjamin.schellinger@web.de>
2025-07-18 14:25:32 +0200

    updated linkedin information

--- sections/about.py
+++ sections/about.py
@@ -69,7 +69,7 @@ def render_about():
             </ul>
 
             When using data, graphs, or insights from the <strong>Crypto Treasury Tracker</strong>, please cite as follows:<br>
-            Crypto Treasury Tracker by Benjamin Schellinger, PhD — url: https://crypto-treasury-tracker.streamlit.app/br>
+            Crypto Treasury Tracker by Benjamin Schellinger, PhD — url: https://crypto-treasury-tracker.streamlit.app</br>
             </p>
 
             <p><strong>Feedback or collaboration?</strong> Connect via <a href="https://www.linkedin.com/in/benjaminschellinger/" target="_blank">LinkedIn</a>.</p>
